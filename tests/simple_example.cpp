7c63f6877e3889580810ae4e1fd56d873207156e
XIAOXIAO WANG <x639wang@uwaterloo.ca>
2019-07-09 15:08:40 -0400

    inital test cases

--- tests/simple_example.cpp
+++ tests/simple_example.cpp
@@ -18,4 +18,25 @@ class vector {
      std::bar(1);
   }
 };
+
+class test1 {
+
+// CHECK-L: void push_back(const T&) ;
+// CHECK-NOT-L: std::bar(1);
+    void push_back(const T&) {
+        std::bar(1);
+    }
+};
 }
+
+namespace testnamespace{
+    class vector {
+
+// CHECK-L: void push_back(const T&) ;
+// CHECK-NOT-L: std::bar(1);
+        void push_back(const T&) {
+            std::bar(1);
+        }
+    };
+}
+
